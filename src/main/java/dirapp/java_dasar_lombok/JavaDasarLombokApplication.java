25ac279ecfb76852cb2ae265a7ff897bfba95f6d
dirasanjayawardana <dirawardana.dw@gmail.com>
2024-08-10 21:13:42 +0700

    backup

---
+++ src/main/java/dirapp/java_dasar_lombok/JavaDasarLombokApplication.java
@@ -0,0 +1,13 @@
+package dirapp.java_dasar_lombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDasarLombokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDasarLombokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dirapp/java_dasar_lombok/JavaDasarLombokApplicationTests.java
@@ -0,0 +1,13 @@
+package dirapp.java_dasar_lombok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDasarLombokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
